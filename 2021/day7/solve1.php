c9528fe5f074f88a2bbae18424140e282f0d3c2e
Quentin <q.ledoledec@dental-monitoring.com>
2021-12-07 08:44:48 +0100

    2021: day7

---
+++ 2021/day7/solve1.php
@@ -0,0 +1,20 @@
+<?php
+
+$input = file(__DIR__ . '/input', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)[0];
+$input = explode(',', $input);
+
+sort($input);
+
+$min_cost = PHP_INT_MAX;
+for ($i = 0; $i <= count($input) - 1; $i++) {
+    $target = $i;
+    $cost = 0;
+    foreach ($input as $position) {
+        $cost+= abs($position - $target);
+    }
+    if ($cost < $min_cost) {
+        $min_cost = $cost;
+    }
+}
+
+echo $min_cost;
